7d4b49a6d67cd1928a1c7cf77110d2317078f60b
Kazuki Ota <kaota@microsoft.com>
2017-06-27 17:05:26 +0900

    Prism template packのものを.net stdに変換

---
+++ BotClient/BotClient/BotClient.Core/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Prism.Autofac;
+using Prism.Autofac.Forms;
+using BotClient.Views;
+using Xamarin.Forms;
+
+namespace BotClient
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            this.InitializeComponent();
+
+            await this.NavigationService.NavigateAsync("NavigationPage/MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            this.Container.RegisterTypeForNavigation<NavigationPage>();
+            this.Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
